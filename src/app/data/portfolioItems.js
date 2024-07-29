06096f70ce7a2938ba953d525ecb0f88530fb119
Hannah Hailu Muthoni <muthonihailuhannah@gmail.com>
2024-07-29 16:47:15 +0300

    Update portfolioItems.js

--- src/app/data/portfolioItems.js
+++ src/app/data/portfolioItems.js
@@ -7,21 +7,21 @@ export const portfolio = [
     {
         id: 1,
         title: 'Tours and Travels App',
-        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/newssite.PNG'},
+        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/tours-and-travels.PNG'},
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?',
         link: 'https://tours-and-travels-github-io.onrender.com'
     },
     {
         id: 2,
         title: 'Coffee Shop App',
-        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/newssite.PNG'},
+        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/coffee-shop.PNG'},
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?',
         link: 'https://coffee-shop-app-github-io.onrender.com'
     },
     {
         id: 3,
         title: 'Food Delivery App',
-        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/newssite.PNG'},
+        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/food-shop.PNG'},
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?',
         link: 'https://food-delivery-app-github-io.onrender.com'
     },
@@ -35,7 +35,7 @@ export const portfolio = [
     {
         id: 5,
         title: 'E-Commerce App',
-        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/newssite.PNG'},
+        image: {src: 'https://hannah-muthoni-hailu.github.io/hannahhailu.github.io/images/e-commerce.PNG'},
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?',
         link: 'https://e-commerce-app-github-io.onrender.com/'
     }
